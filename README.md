0e999aa29ef83b075070cce5757bd21bb4a81fb2
wangzhongjie <UvDream@163.com>
2020-01-18 10:57:53 +0800

    docs📖 更新说明文档

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# git-commit-lint-vscode README
+# git-commit-lint-vscode
+
+在日常的开发中,目前主流的代码管理工具就是 `git` 了,当我们对代码进行改动了,首先得`git commit`提交到本地仓库,`git` 规定了提交时必须填写提交信息作为改动说明,保存 `commit` 历史中,可以找到历史代码,也方便他人 review,还可以输出 CHANGELOG,对项目的研发质量都有很大的提升。
+
+但是在平时的工作中,大部分对于`commit` 都是简单的填写,没有好好的重视,这对于项目管理和维护来说,无疑是不友好的。这个插件就是规范化`git`提交规范,让你的提交不仅"好看"还"实用"
+
+## git 规范提交从何说起?
+
+`git` 规范提交从哪里开始的呢?起源在哪呢?emmmmmm,这就追溯到了`Angular`了!
+让我们看下`Angular`社区的提交规范
+![](static/angular.png)
+
+这个提交记录是不是一目了然?所以`git commit`规范下还是很有必要的!
+
+## 说明
+
+|   类型   | emji |           描述            |
+| :------: | :--: | :-----------------------: |
+|   feat   |  ✨  |        引入新功能         |
+|   fix    |  🐛  |         修复 bug          |
+|  style   |  💄  |    更新 UI 样式文按键     |
+|   docs   |  📖  |       添加/更新文档       |
+|   perf   |  👌  |       提高性能/优化       |
+|   init   |  🎉  |    初次提交/初始化项目    |
+|   test   |  ✅  |       增加测试代码        |
+| refactor |  🎨  |   改进代码结构/代码格式   |
+|  patch   |  🚑  |       添加重要补丁        |
+|   file   |  📦  |        添加新文件         |
+| publish  |  🚀  |        发布新版本         |
+|  config  |  🔧  |       修改配置文件        |
+|   git    |  🙈  | 添加或修改.gitignore 文件 |
+
+## 下载
+
+在 vscode 扩展中搜索 `git-commit-lint-vscode` 即可找到该插件。
+
+## issues
+
+使用中可以在这提问,有什么需求同样可以在这提出来
+
+[https://github.com/UvDream/git-commit-lint-vscode/issues
+
--- src/config/git_emoji_zh.ts
+++ src/config/git_emoji_zh.ts
@@ -2,13 +2,12 @@
  * @Author: wangzhongjie
  * @Date: 2020-01-17 19:47:44
  * @LastEditors  : wangzhongjie
- * @LastEditTime : 2020-01-17 23:06:23
+ * @LastEditTime : 2020-01-18 10:41:04
  * @Description:提交规范
  * @Email: UvDream@163.com
  */
 export interface Emoji {
   readonly emoji: any;
-  readonly entity?: any;
   readonly code: any;
   readonly description: any;
   readonly name: any;
@@ -16,98 +15,84 @@ export interface Emoji {
 let emojis: Array<Emoji> = [
   {
     emoji: "feat✨",
-    entity: "&#x1f525;",
     code: ":fire:",
     description: "引入新功能",
     name: "新功能"
   },
   {
     emoji: "fix🐛",
-    entity: "&#x1f41b;",
     code: ":bug:",
-    description: "修复 bug",
+    description: "修复bug",
     name: "bug"
   },
   {
     emoji: "style💄",
-    entity: "&#x1f525;",
     code: ":lipstick:",
-    description: "更新 UI 和样式文件",
+    description: "更新UI样式文件",
     name: "样式"
   },
   {
     emoji: "docs📖",
-    entity: "&#ff99cc;",
     code: ":lipstick:",
     description: "添加/更新文档",
     name: "文档"
   },
   {
     emoji: "perf👌",
-    entity: "&#x2705;",
     code: ":white_check_mark:",
     description: "提高性能/优化",
     name: "优化"
   },
   {
     emoji: "init🎉",
-    entity: "&#x1f3a8;",
     code: ":tada:",
     description: "初次提交/初始化项目",
     name: "初始化"
   },
   {
     emoji: "test✅",
-    entity: "&#x1f680;",
     code: ":rocket:",
     description: "增加测试代码",
     name: "测试"
   },
   {
     emoji: "refactor🎨",
-    entity: "&#x2728;",
     code: ":sparkles:",
     description: "改进代码结构/代码格式",
     name: "优化"
   },
   {
     emoji: "patch🚑",
-    entity: "&#128657;",
     code: ":ambulance:",
     description: "添加重要补丁",
     name: "补丁"
   },
   {
     emoji: "file📦",
-    entity: "&#x1f4dd;",
     code: ":pencil:",
     description: "添加新文件",
     name: "新文件"
   },
   {
     emoji: "publish🚀",
-    entity: "&#127881;",
     code: ":tada:",
     description: "发布新版本",
     name: "新版本"
   },
   {
-    emoji: "tag🔖",
-    entity: "&#x1f516;",
-    code: ":bookmark:",
+    emoji: "tag📌",
+    code: ":pushpin:",
     description: "发布版本/添加标签",
     name: "书签"
   },
   {
     emoji: "config🔧",
-    entity: "&#x1f527;",
     code: ":wrench:",
     description: "修改配置文件",
     name: "配置"
   },
   {
     emoji: "git🙈",
-    entity: "&#8bdfe7;",
     code: ":see_no_evil:",
     description: "添加或修改.gitignore文件",
     name: "不可见"
