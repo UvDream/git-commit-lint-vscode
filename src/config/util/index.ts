ea2438935a0ad59385a4017c81b304ecc7d97124
wangzhongjie <UvDream@163.com>
2020-01-18 11:07:53 +0800

    docs📖 更新文档

--- src/config/util/index.ts
+++ src/config/util/index.ts
@@ -1,8 +1,8 @@
-import { use_label } from './use-label'
-import { use_emoji } from './use-emoji'
+import { use_label } from "./use-label";
+import { use_emoji } from "./use-emoji";
 
 export const display_method = {
   default: use_emoji,
-  'use label': use_label,
-  'use emoji': use_emoji,
-}
+  "use code": use_label,
+  "use emoji": use_emoji
+};
