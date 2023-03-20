c9ea363b592f28c769ce4bb175a0675bf905be82
ShutDown <uvdream@163.com>
2023-03-20 09:47:37 +0800

    publish🚀: v2.1.0

--- out/utils.js
+++ out/utils.js
@@ -15,11 +15,11 @@ exports.emojiCommit = void 0;
  * @Email: lanrri@163.com
  * @Date: 2023-03-16 14:18:17
  * @Description:
- * @LastEditors: Lanrri
- * @LastEditTime: 2023-03-17 09:56:57
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2023-03-20 09:41:15
  */
 const vscode = require("vscode");
-const DEFAULT_CUSTOM_FORMAT = '${type}${emoji}: ';
+const DEFAULT_CUSTOM_FORMAT = "${type}${emoji}: ";
 const CUSTOM_KEY_REGEX = /\${(\w+)}/g;
 const customItems = (emoji, custom_key) => {
     return emoji.map((item) => {
@@ -29,19 +29,21 @@ const customItems = (emoji, custom_key) => {
         return {
             value,
             label: `${value} ${item.name}`,
-            description: '[' + item.description + ']',
+            description: "[" + item.description + "]",
         };
     });
 };
 // 点击小图标进入插件
 const getGitExtension = () => {
-    const vscodeGit = vscode.extensions.getExtension('vscode.git');
+    const vscodeGit = vscode.extensions.getExtension("vscode.git");
     const gitExtension = vscodeGit && vscodeGit.exports;
     return gitExtension && gitExtension.getAPI(1);
 };
 // 选完填入操作
 const prefixCommit = (repository, prefix) => {
-    const coverInputValue = vscode.workspace.getConfiguration().get('gitCommitLintVscode.coverInputValue');
+    const coverInputValue = vscode.workspace
+        .getConfiguration()
+        .get("gitCommitLintVscode.coverInputValue");
     if (coverInputValue) {
         repository.inputBox.value = `${prefix}`;
         //! 不删除是因为不知道为什么要先清空再赋值，所以注释掉了原代码，测试了一下修改后的代码，感觉也没发现什么问题啊
@@ -54,44 +56,68 @@ const prefixCommit = (repository, prefix) => {
     }
 };
 const emojiCommit = (uri) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     const git = getGitExtension();
     if (!git) {
-        vscode.window.showErrorMessage('无法加载git插件! 请先安装git插件!');
+        vscode.window.showErrorMessage("无法加载git插件! 请先安装git插件!");
         return;
     }
-    const config = vscode.workspace.getConfiguration('gitCommitLintVscode');
-    const custom_format = config.get('customFormat', DEFAULT_CUSTOM_FORMAT);
-    const custom_type = config.get('customType', []);
+    const config = vscode.workspace.getConfiguration("gitCommitLintVscode");
+    const custom_format = config.get("customFormat", DEFAULT_CUSTOM_FORMAT);
+    const custom_type = config.get("customType", []);
     const items = customItems(custom_type, custom_format);
     const selected = yield vscode.window.showQuickPick(items);
     if (!selected) {
         return;
     }
     const { value } = selected;
-    vscode.commands.executeCommand('workbench.scm.focus');
+    vscode.commands.executeCommand("workbench.scm.focus");
     if (git.repositories.length === 1) {
+        const alwaysShowRepositories = vscode.workspace
+            .getConfiguration()
+            .get("scm.alwaysShowRepositories");
         // 确保聚焦到输入框，参考 https://github.com/microsoft/vscode/issues/131006#issuecomment-915751155
-        vscode.commands.executeCommand('list.focusFirst');
-        vscode.commands.executeCommand('list.select');
+        if (alwaysShowRepositories) {
+            // 启用了该配置时，ui会有变动，所以向下移动一格再选中
+            vscode.commands.executeCommand("list.focusFirst");
+            vscode.commands.executeCommand("list.focusDown");
+            vscode.commands.executeCommand("list.select");
+        }
+        else {
+            vscode.commands.executeCommand("list.focusFirst");
+            vscode.commands.executeCommand("list.select");
+        }
         prefixCommit(git.repositories[0], value);
         return;
     }
     if (uri) {
-        const selectedRepositoryIndex = git.repositories.findIndex((repository) => repository.rootUri.path === uri.rootUri.path);
-        const selectedRepository = git.repositories[selectedRepositoryIndex];
-        if (selectedRepository) {
-            prefixCommit(selectedRepository, value);
-            // 存在多个存储库时，关闭其他存储库，定位到选择的存储库并聚焦输入框
-            vscode.commands.executeCommand('list.collapseAll');
-            vscode.commands.executeCommand('list.focusFirst');
-            for (let i = 0; i < selectedRepositoryIndex; i++) {
-                vscode.commands.executeCommand('list.focusDown');
-            }
-            vscode.commands.executeCommand('list.expand');
-            vscode.commands.executeCommand('list.expand');
-            vscode.commands.executeCommand('list.select');
-            vscode.commands.executeCommand('list.toggleExpand');
+        const selectedRepository = git.getRepository(uri.rootUri.path);
+        if (!selectedRepository) {
+            return;
         }
+        prefixCommit(selectedRepository, value);
+        const accurateLocating = vscode.workspace
+            .getConfiguration()
+            .get("gitCommitLintVscode.accurateLocating");
+        if (!accurateLocating) {
+            return;
+        }
+        // 获取在源代码管理存储库的索引值
+        // @ts-ignore
+        const repositoryIndex = (_b = (_a = selectedRepository === null || selectedRepository === void 0 ? void 0 : selectedRepository.repository) === null || _a === void 0 ? void 0 : _a.indexGroup) === null || _b === void 0 ? void 0 : _b.t;
+        if (!repositoryIndex) {
+            return;
+        }
+        // 存在多个存储库时，关闭其他存储库，定位到选择的存储库并聚焦输入框
+        vscode.commands.executeCommand("list.collapseAll");
+        vscode.commands.executeCommand("list.focusFirst");
+        for (let i = 0; i < repositoryIndex; i++) {
+            vscode.commands.executeCommand("list.focusDown");
+        }
+        vscode.commands.executeCommand("list.expand");
+        vscode.commands.executeCommand("list.expand");
+        vscode.commands.executeCommand("list.select");
+        vscode.commands.executeCommand("list.toggleExpand");
     }
     else {
         for (let repo of git.repositories) {
--- src/utils.ts
+++ src/utils.ts
@@ -3,39 +3,44 @@
  * @Email: lanrri@163.com
  * @Date: 2023-03-16 14:18:17
  * @Description:
- * @LastEditors: FreezeNow
- * @LastEditTime: 2023-03-19 18:50:57
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2023-03-20 09:41:15
  */
-import * as vscode from 'vscode';
-import { GitExtension, Repository } from './git';
-import { CustomType } from './type';
+import * as vscode from "vscode";
+import { GitExtension, Repository } from "./git";
+import { CustomType } from "./type";
 
-const DEFAULT_CUSTOM_FORMAT = '${type}${emoji}: ';
+const DEFAULT_CUSTOM_FORMAT = "${type}${emoji}: ";
 const CUSTOM_KEY_REGEX = /\${(\w+)}/g;
 
 const customItems = (emoji: CustomType[], custom_key: string) => {
   return emoji.map((item) => {
-    const value = custom_key.replace(CUSTOM_KEY_REGEX, (_match, variable: keyof CustomType) => {
-      return item[variable];
-    });
+    const value = custom_key.replace(
+      CUSTOM_KEY_REGEX,
+      (_match, variable: keyof CustomType) => {
+        return item[variable];
+      }
+    );
     return {
       value,
       label: `${value} ${item.name}`,
-      description: '[' + item.description + ']',
+      description: "[" + item.description + "]",
     };
   });
 };
 
 // 点击小图标进入插件
 const getGitExtension = () => {
-  const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
+  const vscodeGit = vscode.extensions.getExtension<GitExtension>("vscode.git");
   const gitExtension = vscodeGit && vscodeGit.exports;
   return gitExtension && gitExtension.getAPI(1);
 };
 
 // 选完填入操作
 const prefixCommit = (repository: Repository, prefix: String) => {
-  const coverInputValue = vscode.workspace.getConfiguration().get('gitCommitLintVscode.coverInputValue');
+  const coverInputValue = vscode.workspace
+    .getConfiguration()
+    .get("gitCommitLintVscode.coverInputValue");
   if (coverInputValue) {
     repository.inputBox.value = `${prefix}`;
     //! 不删除是因为不知道为什么要先清空再赋值，所以注释掉了原代码，测试了一下修改后的代码，感觉也没发现什么问题啊
@@ -51,14 +56,14 @@ const emojiCommit = async (uri?: { rootUri: { path: any } }) => {
   const git = getGitExtension();
 
   if (!git) {
-    vscode.window.showErrorMessage('无法加载git插件! 请先安装git插件!');
+    vscode.window.showErrorMessage("无法加载git插件! 请先安装git插件!");
     return;
   }
 
-  const config = vscode.workspace.getConfiguration('gitCommitLintVscode');
+  const config = vscode.workspace.getConfiguration("gitCommitLintVscode");
 
-  const custom_format = config.get('customFormat', DEFAULT_CUSTOM_FORMAT);
-  const custom_type = config.get('customType', []);
+  const custom_format = config.get("customFormat", DEFAULT_CUSTOM_FORMAT);
+  const custom_type = config.get("customType", []);
 
   const items = customItems(custom_type, custom_format);
   const selected = await vscode.window.showQuickPick(items);
@@ -66,19 +71,21 @@ const emojiCommit = async (uri?: { rootUri: { path: any } }) => {
     return;
   }
   const { value } = selected;
-  vscode.commands.executeCommand('workbench.scm.focus');
+  vscode.commands.executeCommand("workbench.scm.focus");
 
   if (git.repositories.length === 1) {
-    const alwaysShowRepositories = vscode.workspace.getConfiguration().get('scm.alwaysShowRepositories');
+    const alwaysShowRepositories = vscode.workspace
+      .getConfiguration()
+      .get("scm.alwaysShowRepositories");
     // 确保聚焦到输入框，参考 https://github.com/microsoft/vscode/issues/131006#issuecomment-915751155
     if (alwaysShowRepositories) {
       // 启用了该配置时，ui会有变动，所以向下移动一格再选中
-      vscode.commands.executeCommand('list.focusFirst');
-      vscode.commands.executeCommand('list.focusDown');
-      vscode.commands.executeCommand('list.select');
+      vscode.commands.executeCommand("list.focusFirst");
+      vscode.commands.executeCommand("list.focusDown");
+      vscode.commands.executeCommand("list.select");
     } else {
-      vscode.commands.executeCommand('list.focusFirst');
-      vscode.commands.executeCommand('list.select');
+      vscode.commands.executeCommand("list.focusFirst");
+      vscode.commands.executeCommand("list.select");
     }
     prefixCommit(git.repositories[0], value);
     return;
@@ -89,25 +96,28 @@ const emojiCommit = async (uri?: { rootUri: { path: any } }) => {
       return;
     }
     prefixCommit(selectedRepository, value);
-    const accurateLocating = vscode.workspace.getConfiguration().get('gitCommitLintVscode.accurateLocating');
+    const accurateLocating = vscode.workspace
+      .getConfiguration()
+      .get("gitCommitLintVscode.accurateLocating");
     if (!accurateLocating) {
       return;
     }
     // 获取在源代码管理存储库的索引值
-    const repositoryIndex = selectedRepository?.repository?.indexGroup?.t ;
+    // @ts-ignore
+    const repositoryIndex = selectedRepository?.repository?.indexGroup?.t;
     if (!repositoryIndex) {
       return;
     }
     // 存在多个存储库时，关闭其他存储库，定位到选择的存储库并聚焦输入框
-    vscode.commands.executeCommand('list.collapseAll');
-    vscode.commands.executeCommand('list.focusFirst');
+    vscode.commands.executeCommand("list.collapseAll");
+    vscode.commands.executeCommand("list.focusFirst");
     for (let i = 0; i < repositoryIndex; i++) {
-      vscode.commands.executeCommand('list.focusDown');
+      vscode.commands.executeCommand("list.focusDown");
     }
-    vscode.commands.executeCommand('list.expand');
-    vscode.commands.executeCommand('list.expand');
-    vscode.commands.executeCommand('list.select');
-    vscode.commands.executeCommand('list.toggleExpand');
+    vscode.commands.executeCommand("list.expand");
+    vscode.commands.executeCommand("list.expand");
+    vscode.commands.executeCommand("list.select");
+    vscode.commands.executeCommand("list.toggleExpand");
   } else {
     for (let repo of git.repositories) {
       prefixCommit(repo, value);
